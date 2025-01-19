21b782e05d6b21211d1dc2d2eeb667e755826ecc
whitewolf <ahsanauddry.ndc@gmail.com>
2025-01-19 12:27:32 +0600

    NavbarAdded

---
+++ imageSlider.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="index.css">
+    <link rel="stylesheet" href="index.js">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    <title>ATMABISWAS</title>
+</head>
+<body>
+    <div class="slider-container">
+        <div class="left-slide">
+            <div style="background-color: #fde">
+                <h1>Pink Cherrish</h1>
+                <p>to evolve into care</p>
+            </div>
+            <div style="background-color: rgb(254,160,0)">
+                <h1>Yellow color</h1>
+                <p>Shinning the clouds</p>
+            </div>
+            <div style="background-color: rgb(231, 58, 95)">
+                <h1>Redness Lust</h1>
+                <p>in the wilderness</p>
+            </div>
+            <div style="background-color: rgb(16, 68, 73)">
+                <h1>Lavanda Love</h1>
+                <p>in the sunset</p>
+            </div>
+        </div>
+        <div class="right-slide">
+            <div style="background-image: url('https://letsenhance.io/static/8f5e523ee6b2479e26ecc91b9c25261e/1015f/MainAfter.jpg');"></div>
+            <div style="background-image: url('https://gratisography.com/wp-content/uploads/2024/11/gratisography-augmented-reality-800x525.jpg');"></div>
+            <div style="background-image: url('https://images.unsplash.com/photo-1490750967868-88aa4486c946?ixid=MXwxMjA3fDB8MHxzZWFyY2h8M3x8Zmxvd2Vyc3xlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1296&q=60');"></div>
+            <div style="background-image: url('https://images.unsplash.com/photo-1462275646964-a0e3386b89fa?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1400&q=80');"></div>
+        </div>
+        <div class="action-buttons">
+            <button class="down-button">
+                <i class="fas fa-arrow-down"></i>
+            </button>
+            <button class="up-button">
+                <i class="fas fa-arrow-up"></i>
+            </button>
+        </div>
+    </div>
+<script src="index.js" > </script>
+</body>
+</html>
