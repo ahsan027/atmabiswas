aae6ac6448e8e088791fe000b0494d3844896892
whitewolf <ahsanauddry.ndc@gmail.com>
2025-01-28 02:14:12 +0600

    ManyMajorChanges

--- Contact.php
+++ Contact.php
@@ -16,6 +16,17 @@ $conn = $db->connect();
 
     <link rel="stylesheet" href="locations.css">
 
+<style>
+    *{
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+body{
+    background-color: #e6f2ff;
+}
+</style>
+    
 
 
 </head>
@@ -30,18 +41,25 @@ $conn = $db->connect();
     <!-- Contact Card -->
     <div class="contact-card" id="contact-card">
         <div class="contact-info">
-            <h2>ATMABISWAS HQ</h2>
+            <div class="cinfos">
+                            <h2>ATMABISWAS HQ</h2>
             <p><strong>Branch:</strong> ATMABISWAS, Main Office</p>
             <p><strong>Address:</strong> 123 Main Street, Dhaka, Bangladesh</p>
             <p><strong>Phone:</strong> +880-123-456-789</p>
             <p><strong>Email:</strong> contact@atmabiswas.org</p>
+            </div>
+            <div class="btn">
+                        <a href="store.php"><button id="">ATMABISWAS Locations</button></a>
         </div>
+
+        </div>
+        
         <div class="map-container">
             <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3654.990205551057!2d88.84420999999999!3d23.640521800000002!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39fecc6a98f15555%3A0x7237da8c2d53a42d!2sAtmabiswas!5e0!3m2!1sen!2sbd!4v1737729888492!5m2!1sen!2sbd"  loading="lazy" allowfullscreen loading="lazy"></iframe>
                 
         </div>
     </div>
-            <a href="store.php"><button class="toggle-btn" id="">ATMABISWAS Locations</button></a>
+
         <button class="toggle-btn" id="filterbutton">ATMABISWAS Branches</button>
     <div class="filterbars" id="filterbars">
       
