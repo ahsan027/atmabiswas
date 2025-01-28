8f1dce25a768b2d0aba1d7475613bab26f5fb534
whitewolf <ahsanauddry.ndc@gmail.com>
2025-01-28 17:45:07 +0600

    LocationDone

--- allLocations.js
+++ allLocations.js
@@ -1,16 +1,32 @@
-console.log("Hi");
-
 window.onload = function () {
   getData();
-  console.log("Hi");
 };
 function getData() {
   axios
     .get("locations.json")
-    .then((response) =>
+    .then((response) => {
+      const container = document.getElementById("storeId");
+
       response.data.forEach((element, i) => {
-        console.log(i + 1, element.code);
-      })
-    )
+        const link = document.createElement("a");
+
+        link.href = "#";
+        link.className = "list-group-item list-group-item-action";
+        link.setAttribute("data-lat", element.latitude);
+        link.setAttribute("data-lng", element.longitude);
+
+        link.innerHTML = `<small>Branch code: ${element.code}</small><br>
+        <h5 class="mb-1">${element.branch_name}</h5>
+                            <p class="mb-1">Address: ${element.address}</p>
+                            <small>Phone: ${element.mobile}</small><br>
+                            <small>Division: ${element.division}, District: ${element.district}</small>`;
+        link.addEventListener("click", (e) => {
+          e.preventDefault();
+          moveToLocation(element.latitude, element.longitude);
+        });
+        console.log(element);
+        container.appendChild(link);
+      });
+    })
     .catch((e) => console.log(e));
 }
