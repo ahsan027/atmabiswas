1976eb8e70c2d5cafefd1d90c044816c549a0a5c
ahsanauddry027 <ahsan.habib1@g.bracu.ac.bd.com>
2025-02-09 23:55:28 +0600

    PushContents

---
+++ press.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Press - ATMABISWAS </title>
+  <link rel="stylesheet" href="green.css">
+</head>
+
+<body>
+  <?php include 'Navbar.php' ?>
+  <header>
+    <h1>This page is Under Construction...</h1>
+    <p>Sorry for the delay, Please Be patient</p>
+    <p><strong>{Life is what happens when you're busy making other plans. - John Lennon}</strong></p>
+  </header>
+
+  <?php include 'footer.php' ?>
+</body>
+
+</html>
