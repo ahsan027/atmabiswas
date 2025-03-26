ddb7bf3afd2790e28be786178ae0864e01a8d1f6
whitewolf <ahsanauddry.ndc@gmail.com>
2025-03-26 12:55:13 +0600

    puahLL

--- navbar.js
+++ navbar.js
@@ -1,16 +1,3 @@
-console.log(window.location.href);
-
-document
-  .getElementById("login-btn")
-  .addEventListener("click", function (event) {
-    event.preventDefault();
-    document.getElementById("login-popup").classList.add("active");
-  });
-
-document.getElementById("close-popup").addEventListener("click", function () {
-  document.getElementById("login-popup").classList.remove("active");
-});
-
 document.addEventListener("DOMContentLoaded", function () {
   const currentLocation = location.href;
   console.log(location.href);
