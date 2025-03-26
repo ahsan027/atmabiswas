dd930aec8c743d82a3965646e7437bed61deead2
whitewolf <ahsanauddry.ndc@gmail.com>
2025-03-26 13:51:15 +0600

    newContents

--- frontend/career.php
+++ frontend/career.php
@@ -15,7 +15,7 @@
                 <img src="https://atmabiswas.org/wp-content/uploads/2024/10/cropped-Monogram-web.webp" alt="Logo">
             </div>
             <ul class="menu">
-                <li><a href="/">Home</a></li>
+                <li><a href="/frontend/index.php">Home</a></li>
                 <li><a href="#">Available Jobs</a></li>
                 <li><a href="#">Login</a></li>
             </ul>
--- frontend/navbar.js
+++ frontend/navbar.js
@@ -9,3 +9,14 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   });
 });
+document.addEventListener("DOMContentLoaded", function () {
+  const currentLocation = location.href;
+  console.log(location.href);
+  const menuItems = document.querySelectorAll(".navbar .top-row .bars a");
+
+  menuItems.forEach((item) => {
+    if (item.href === currentLocation) {
+      item.classList.add("active");
+    }
+  });
+});
