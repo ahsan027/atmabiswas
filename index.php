df40129ef8e1b3d874c9ad09cdf02913a415f717
whitewolf <ahsanauddry.ndc@gmail.com>
2025-03-26 09:17:42 +0600

    Changes

--- index.php
+++ index.php
@@ -4,19 +4,18 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<<<<<<< HEAD
-    <title>Responsive Navbar with Active Page</title>
-=======
-    <title>ATMA BISWAS</title>
-    <link rel="icon" type="image/png" href="LOGO/NGO_logo_monogram.png">
->>>>>>> d3142c1382bf0d36da4107189e65d5170eb32513
+    <<<<<<< HEAD <title>Responsive Navbar with Active Page</title>
+        =======
+        <title>ATMA BISWAS</title>
+        <link rel="icon" type="image/png" href="LOGO/NGO_logo_monogram.png">
+        >>>>>>> d3142c1382bf0d36da4107189e65d5170eb32513
 
 
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css"
-        integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg=="
-        crossorigin="anonymous" referrerpolicy="no-referrer" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css"
+            integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg=="
+            crossorigin="anonymous" referrerpolicy="no-referrer" />
 
-    <link rel="stylesheet" href="index.css">
+        <link rel="stylesheet" href="index.css">
 </head>
 
 <body>
@@ -29,7 +28,6 @@
         <div class="numbers">
             <div class="number-card">
                 <h2 id="number1">0</h2>
-<<<<<<< HEAD
                 <p>Projects Completed</p>
             </div>
             <div class="number-card">
@@ -43,29 +41,14 @@
             <div class="number-card">
                 <h2 id="number4">0</h2>
                 <p>Cups of Coffee</p>
-=======
-                <p>Years of Foundation</p>
-            </div>
-            <div class="number-card">
-                <h2 id="number2">0</h2>
-                <p>Projects Completed</p>
-            </div>
-            <div class="number-card">
-                <h2 id="number3">0 <span>K</span>
-                </h2>
-                <p>Registered Members</p>
-            </div>
-            <div class="number-card">
-                <h2 id="number4">0</h2>
-                <p>Partner's Organizatioin</p>
->>>>>>> d3142c1382bf0d36da4107189e65d5170eb32513
             </div>
         </div>
     </div>
 
     <div class="sectionalparts">
         <h1>Our Goals</h1>
-        <p>Our mission is to work for progressive social transformation with the aim of institutionalizing a society that place <strong>harmony, peace, justice and ecological balance together</strong></p>
+        <p>Our mission is to work for progressive social transformation with the aim of institutionalizing a society
+            that place <strong>harmony, peace, justice and ecological balance together</strong></p>
 
 
     </div>
