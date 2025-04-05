aa7a6037fd9ea9bf2c4c766c79f64480d3289537
ahsanauddry027 <ahsan.habib1@g.bracu.ac.bd.com>
2025-04-05 13:56:00 +0600

    newFilesAdded

--- backend/DashBoard/blog.php
+++ backend/DashBoard/blog.php
@@ -19,7 +19,7 @@
   <div class="container">
     <div class="card editor-section">
       <h1>ATMABISWAS Blog Editor</h1>
-      <form action="">
+      <form action="../blogUpload_process.php" method="POST">
         <input type="text" id="blogTitle" placeholder="Blog Title" />
         <select id="blogCategory">
           <option value="">Select Category</option>
