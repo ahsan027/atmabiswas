28b1179fb56e7bae6e6c9f336c7033419a972f84
ahsanauddry027 <ahsan.habib1@g.bracu.ac.bd.com>
2025-04-07 10:53:44 +0600

    SomeChanges

--- backend/login/logingggg.php
+++ backend/login/logingggg.php
@@ -1,134 +1,27 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login Form Validation</title>
+    <title>ATMABISWAS - Login</title>
     <!-- Font Awesome CDN -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css">
-    <!-- Inline CSS -->
-    <style>
-        @import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap');
-
-        * {
-            margin: 0;
-            padding: 0;
-            box-sizing: border-box;
-        }
-
-        body {
-            font-family: "Montserrat", sans-serif;
-            background: linear-gradient(rgba(0, 0, 0, .5), rgba(0, 0, 0, .5)), url("images/logincover.jpg");
-            background-size: cover;
-            background-position: center;
-        }
-
-        .box {
-            border-radius: 20px;
-            width: 700px;
-            padding: 30px;
-            background-color: rgba(255, 255, 255, 0.1);
-            box-shadow: 0 8px 32px rgba(0, 0, 0, 10);
-            display: flex;
-            flex-direction: column;
-            backdrop-filter: blur(3px);
-        }
-
-        .box h1 {
-            color: #fff;
-            text-transform: uppercase;
-            font-weight: 700;
-            display: flex;
-            align-items: center;
-            justify-content: center;
-        }
-
-        .username,
-        .password {
-            position: relative;
-        }
-
-        .fa-regular.fa-envelope,
-        .fa-solid.fa-lock {
-            position: absolute;
-            left: 5px;
-            top: 13px;
-            color: rgb(256, 256, 256);
-        }
-        .fa-regular.fa-envelope,
-        .fa-solid.fa-lock {
-            font-size: 2rem;
-            color: rgb(256, 256, 256);
-        }
-        .box input[type="text"],
-        .box input[type="password"] {
-            font-size: 20px;
-            border: none;
-            background: none;
-            display: block;
-            margin: 30px auto;
-            text-align: center;
-            border: 3px solid #fff;
-            padding: 14px 10px;
-            width: 550px;
-            color: #fff;
-            border-radius: 24px;
-            transition: all .2s ease-in;
-            outline: none;
-        }
-
-        .box input[type="text"]:focus,
-        .box input[type="password"]:focus {
-            border-color: #0367fd;
-        }
-
-        .wrapper {
-            display: flex;
-            height: 100vh;
-            align-items: center;
-            justify-content: center;
-        }
-
-        #btn {
-            background: none;
-            display: block;
-            margin: 20px auto;
-            text-align: center;
-            border: none;
-            padding: 14px 35px;
-            outline: none;
-            font-size: 18px;
-            font-weight: 700;
-            color: rgb(256, 256, 256);
-            border-radius: 20px;
-            transition: all .2s ease-in-out;
-            cursor: pointer;
-            background-color: #0367fd;
-        }
-
-        #btn:hover {
-            background-color: #025edc;
-        }
-        img {
-            width: 100px;
-            height: 100px;
-            margin: 0px auto;
-            display: block;
-        }
-    </style>
+    <link rel="stylesheet" href="css/login.css">
 </head>
+
 <body>
     <div class="wrapper">
         <div class="box">
             <form action="">
-            <img src="../images/logo/logo.png">
-                <h1>Login</h1>
+                <img src="../images/logo/logo.png">
+                <h1>Welcome to, ATMABISWAS Dashboard</h1>
                 <div class="username">
-                    <input type="text" placeholder="Enter Email Address" id="username" autocomplete="off" required>
+                    <input type="text" placeholder="Enter Email Address" id="username" required>
                     <i class="fa-regular fa-envelope"></i>
                 </div>
                 <div class="password">
-                    <input type="password" placeholder="Enter password" id="password" autocomplete="off" required>
+                    <input type="password" placeholder="Enter password" id="password" required>
                     <i class="fa-solid fa-lock"></i>
                 </div>
                 <button id="btn" type="submit">Login</button>
@@ -137,4 +30,5 @@
         </div>
     </div>
 </body>
+
 </html>
