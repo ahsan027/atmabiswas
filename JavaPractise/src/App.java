8a1c2f75cf1b9afd177f3c89e39e29ddae7d0191
ahsanauddry027 <ahsan.habib1@g.bracu.ac.bd.com>
2025-04-09 16:03:10 +0600

    Changes

--- JavaPractise/src/App.java
+++ JavaPractise/src/App.java
@@ -1,5 +1,20 @@
+class Recursion {
+
+    public void printNuM(int num) {
+        if (num == 1) {
+            System.out.println(num);
+            return;
+        }
+        System.out.print(num + " ");
+        printNuM(num - 1);
+    }
+}
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        int n = 10;
+        Recursion newRe = new Recursion();
+        newRe.printNuM(n);
     }
 }
