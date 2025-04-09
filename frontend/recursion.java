3d734a79710a178d1e099ceccaadbdef3e019fe3
ahsanauddry027 <ahsan.habib1@g.bracu.ac.bd.com>
2025-04-09 15:57:05 +0600

    PushAll

---
+++ frontend/recursion.java
@@ -0,0 +1,16 @@
+package frontend;
+
+public class Recursion {
+
+  public static void printNum(int i) {
+    if (i == 1) {
+      return;
+    }
+  }
+
+}
+
+public static void main(String args[]) {
+  int n = 10;
+  Recursion newRecursion = new Recursion();
+}
