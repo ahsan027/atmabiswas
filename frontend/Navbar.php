f60c1d33598d5e88262f58d3cec054e09a033daf
OkArafat <arafatbiswas.edu01@gmail.com>
2025-06-03 14:03:58 +0300

    add new

--- frontend/Navbar.php
+++ frontend/Navbar.php
@@ -61,46 +61,46 @@
 <div class="sidenav">
     <div class="sidelogo">
         <img src="LOGO/Monogram for web only.png" alt="Logo" class="profile-img">
-        <i id="close-btn" class="fa-solid fa-x"></i>
+        <i id="close-btn" class="fa-solid fa-times"></i>
     </div>
-    <a href="/">Who we are</a>
+    <a href="/"><i class="fa-solid fa-house-user"></i> Who we are</a>
     <div class="sidedrop">
         <div class="mainsidedrop">
-            <a href="#">Our Team <i id="arrow" class="fa-solid fa-caret-down"></i></a>
+            <a href="#"><i class="fa-solid fa-people-group"></i> Our Team <i id="arrow" class="fa-solid fa-caret-down"></i></a>
         </div>
         <div class="sidedropContent">
-            <a href="eve.php">Executive</a>
-            <a href="generalbody.php">General Body</a>
-            <a href="SeniorManagement.php">Senior Management</a>
-            <a href="founder.php">Founder</a>
-        </div>
-    </div>
-    <div class="sidedrop">
-        <div class="mainsidedrop">
-            <a href="#">What we do <i id="arrow" class="fa-solid fa-caret-down"></i></a>
-        </div>
-        <div class="sidedropContent">
-            <a href="Green_Energy.php">Green Energy</a>
-            <a href="enterprice.php">Enterprise Developement</a>
-            <a href="Agritural.php">Food & Agriculture</a>
-            <a href="readytoeat.php">Ready To Eat</a>
-            <a href="health.php">Health & Nutrition</a>
+            <a href="eve.php"><i class="fa-solid fa-user-tie"></i> Executive</a>
+            <a href="generalbody.php"><i class="fa-solid fa-users"></i> General Body</a>
+            <a href="SeniorManagement.php"><i class="fa-solid fa-user-shield"></i> Senior Management</a>
+            <a href="founder.php"><i class="fa-solid fa-user"></i> Founder</a>
         </div>
     </div>
     <div class="sidedrop">
         <div class="mainsidedrop">
-            <a href="#">Activities <i id="arrow" class="fa-solid fa-caret-down"></i></a>
+            <a href="#"><i class="fa-solid fa-clipboard-list"></i> What we do <i id="arrow" class="fa-solid fa-caret-down"></i></a>
         </div>
         <div class="sidedropContent">
-            <a href="career.php">Career</a>
-            <a href="notice.php">Notice</a>
-            <a href="press.php">Press</a>
-            <a href="aboutus.php">About Us</a>
+            <a href="Green_Energy.php"><i class="fa-solid fa-leaf"></i> Green Energy</a>
+            <a href="enterprice.php"><i class="fa-solid fa-building"></i> Enterprise Developement</a>
+            <a href="Agritural.php"><i class="fa-solid fa-seedling"></i> Food & Agriculture</a>
+            <a href="readytoeat.php"><i class="fa-solid fa-pizza-slice"></i> Ready To Eat</a>
+            <a href="health.php"><i class="fa-solid fa-stethoscope"></i> Health & Nutrition</a>
         </div>
     </div>
-    <a href="Events.php">Events</a>
-    <a href="social.php">Social</a>
-    <a href="Contact.php">Contacts</a>
+    <div class="sidedrop">
+        <div class="mainsidedrop">
+            <a href="#"><i class="fa-solid fa-chart-line"></i> Activities <i id="arrow" class="fa-solid fa-caret-down"></i></a>
+        </div>
+        <div class="sidedropContent">
+            <a href="career.php"><i class="fa-solid fa-briefcase"></i> Career</a>
+            <a href="notice.php"><i class="fa-solid fa-bullhorn"></i> Notice</a>
+            <a href="press.php"><i class="fa-solid fa-newspaper"></i> Press</a>
+            <a href="aboutus.php"><i class="fa-solid fa-circle-info"></i> About Us</a>
+        </div>
+    </div>
+    <a href="Events.php"><i class="fa-solid fa-calendar-check"></i> Events</a>
+    <a href="social.php"><i class="fa-solid fa-share-nodes"></i> Social</a>
+    <a href="Contact.php"><i class="fa-solid fa-envelope-open-text"></i> Contacts</a>
 </div>
 
 <script src="navbar.js"></script>
--- frontend/menutoggle.js
+++ frontend/menutoggle.js
@@ -2,6 +2,9 @@ const menuToggle = document.getElementById("menu-toggleId");
 const sideNav = document.querySelector(".sidenav");
 const closeBtn = document.getElementById("close-btn");
 const sideDropdowns = document.querySelectorAll(".sidenav .sidedrop");
+const sideNavLinks = document.querySelectorAll(
+  ".sidenav a:not(.mainsidedrop a)"
+); // Select all links EXCEPT the main dropdown toggles
 
 menuToggle.addEventListener("click", () => {
   sideNav.classList.toggle("active");
@@ -14,7 +17,12 @@ closeBtn.addEventListener("click", () => {
   menuToggle.classList.remove("active");
 });
 
-// Close the sidenav when clicking outside of it (optional, but good for UX)
+// Prevent clicks inside the sidebar from closing it via the document listener
+sideNav.addEventListener("click", (event) => {
+  event.stopPropagation();
+});
+
+// Close the sidenav when clicking outside of it
 document.addEventListener("click", (event) => {
   const isClickInsideSidenav = sideNav.contains(event.target);
   const isClickOnMenuToggle = menuToggle.contains(event.target);
@@ -32,10 +40,17 @@ document.addEventListener("click", (event) => {
 // Toggle dropdowns within the sidenav
 sideDropdowns.forEach((dropdown) => {
   const mainDropdown = dropdown.querySelector(".mainsidedrop");
-  const dropdownContent = dropdown.querySelector(".sidedropContent");
 
   mainDropdown.addEventListener("click", (event) => {
     event.preventDefault(); // Prevent default link behavior
     dropdown.classList.toggle("active"); // Toggle active class on the parent .sidedrop
+  });
+});
+
+// Close the sidenav when a navigation link is clicked
+sideNavLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    sideNav.classList.remove("active");
+    menuToggle.classList.remove("active");
   });
 });
