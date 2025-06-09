3919fb964a8f8107577f45a2add00054ea6617b2
whitewolf <ahsanauddry.ndc@gmail.com>
2025-06-09 14:42:35 +0600

    Changes

--- backend/deleteJobPositions.php
+++ backend/deleteJobPositions.php
@@ -1,3 +1,21 @@
 <?php
 
-echo $_GET["job_id"];
+include "Database/db.php";
+
+$database = new Db();
+
+$conn = $database->connect();
+
+$jobId = htmlspecialchars($_GET["job_id"]);
+
+$sql = "DELETE FROM jobcodes WHERE jobid= :jobid";
+
+$stmt = $conn->prepare($sql);
+
+$stmt->bindParam(":jobid", $jobId);
+
+if ($stmt->execute()) {
+    header("Location: Dashboard/dashboard.php");
+} else {
+    echo "Unable to delete Job position of id {$jobId}";
+}
