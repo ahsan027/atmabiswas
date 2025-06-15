ceec2fa67d5b6a608c4948dba8f1cee58fa3345a
ahsan027 <ahsanauddry.ndc@gmail.com>
2025-06-15 14:22:35 +0600

    C

--- frontend/press.php
+++ frontend/press.php
@@ -32,9 +32,11 @@ if ($article_id !== null && isset($press_items[$article_id])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ATMABISWAS Press</title>
+    <title>Press - ATMABISWAS </title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <link rel="stylesheet" href="press.css">
+    
+    <link rel="icon" type="image/png" href="LOGO/NGO_logo_monogram.png">
 </head>
 
 <body>
